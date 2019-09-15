0aebebfa3e8e4e5a237426de8a7ccf2c9777d4bc
Vitaly Chikunov <vt@altlinux.org>
2019-09-15 06:34:49 +0300

    1.0.5-alt1
    - Compatibility with kernels up to 5.2.
    - Add audit record.
    - Multi-pass erase with specified patterns.

--- dm-secdel.c
+++ dm-secdel.c
@@ -22,7 +22,7 @@
 MODULE_AUTHOR("<vt@altlinux.org>");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("dm-linear with secure deletion on discard");
-MODULE_VERSION("1.0.4");
+MODULE_VERSION("1.0.5");
 
 #define DM_MSG_PREFIX "secdel"
 
