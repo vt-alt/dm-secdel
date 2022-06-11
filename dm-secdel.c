6940a2f81212b4bcd04cbaceb6f50e161baa6dfc
Vitaly Chikunov <vt@altlinux.org>
2022-06-11 12:16:53 +0300

    Build for v5.17: Handle CONFIG_FS_DAX as CONFIG_DAX_DRIVER
    Corresponds to 5d2a228b9e13 ("dm: make the DAX support depend on
    CONFIG_FS_DAX").

--- dm-secdel.c
+++ dm-secdel.c
@@ -437,7 +437,7 @@ static void secdel_io_hints(struct dm_target *ti, struct queue_limits *limits)
 	limits->max_discard_sectors = PAGE_SIZE >> 9;
 }
 
-#if IS_ENABLED(CONFIG_DAX_DRIVER)
+#if IS_ENABLED(CONFIG_DAX_DRIVER) || IS_ENABLED(CONFIG_FS_DAX)
 static struct dax_device *secdel_dax_pgoff(struct dm_target *ti, pgoff_t *pgoff)
 {
 	struct secdel_c *lc = ti->private;
