vt-alt/dm-secdel

commit 6d8f9f8e97dfe00a32dbda6dcd1106955c7b2363

Compatibility with v4.13 and v4.14
Offset commits:
  4e4cbee93d56137ebff722be022cae5f70ef84fb v4.13
  74d46992e0d9dee7f1f376de0d56d31614c8a17a v4.14

dm-secdel.c
@@ -86,7 +86,7 @@ static int secdel_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 
 static void secdel_dtr(struct dm_target *ti)
 {
-	struct secdel_c *lc = (struct secdel_c *) ti->private;
+	struct secdel_c *lc = ti->private;
 
 	dm_put_device(ti, lc->dev);
 	kfree(lc);
@@ -99,11 +99,15 @@ static sector_t secdel_map_sector(struct dm_target *ti, sector_t bi_sector)
 	return lc->start + dm_target_offset(ti, bi_sector);
 }
 
+#ifndef bio_set_dev
+#define bio_set_dev(bio, x) (bio)->bi_bdev = (x)
+#endif
+
 static void secdel_map_bio(struct dm_target *ti, struct bio *bio)
 {
 	struct secdel_c *lc = ti->private;
 
-	bio->bi_bdev = lc->dev->bdev;
+	bio_set_dev(bio, lc->dev->bdev);
 	if (bio_sectors(bio)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0)
 	    || bio_op(bio) == REQ_OP_ZONE_RESET
@@ -113,16 +117,19 @@ static void secdel_map_bio(struct dm_target *ti, struct bio *bio)
 			secdel_map_sector(ti, bio->bi_iter.bi_sector);
 }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,13,0)
+#define bi_status bi_error
+#endif
 static void bio_end_erase(struct bio *bio)
 {
 	struct bio_vec *bvec;
 	int i;
 
-	if (bio->bi_error)
+	if (bio->bi_status)
 		DMERR("bio_end_erase %lu[%u] error=%d",
 		    bio->bi_iter.bi_sector,
 		    bio->bi_iter.bi_size >> 9,
-		    bio->bi_error);
+		    bio->bi_status);
 	bio_for_each_segment_all(bvec, bio, i)
 		if (bvec->bv_page != ZERO_PAGE(0))
 			__free_page(bvec->bv_page);
@@ -131,7 +138,7 @@ static void bio_end_erase(struct bio *bio)
 
 static void secdel_submit_bio(struct bio *bio)
 {
-#ifdef bio_set_op_attrs
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,8,0)
 	bio_set_op_attrs(bio, REQ_OP_WRITE, 0);
 	submit_bio(bio);
 #else
@@ -178,7 +185,7 @@ static int issue_erase(struct block_device *bdev, sector_t sector,
 		}
 
 		bio->bi_iter.bi_sector = sector;
-		bio->bi_bdev   = bdev;
+		bio_set_dev(bio, bdev);
 		bio->bi_end_io = bio_end_erase;
 
 		while (nr_sects != 0) {
@@ -218,7 +225,8 @@ static int issue_erase(struct block_device *bdev, sector_t sector,
 /* convert discards into writes */
 static int secdel_map_discard(struct dm_target *ti, struct bio *sbio)
 {
-	struct block_device *bi_bdev = sbio->bi_bdev;
+	struct secdel_c *lc = ti->private;
+	struct block_device *bdev = lc->dev->bdev;
 	sector_t sector = sbio->bi_iter.bi_sector;
 	sector_t nr_sects = bio_sectors(sbio);
 
@@ -234,7 +242,7 @@ static int secdel_map_discard(struct dm_target *ti, struct bio *sbio)
 
 	bio_endio(sbio);
 
-	issue_erase(bi_bdev, sector, nr_sects, GFP_NOFS, 1);
+	issue_erase(bdev, sector, nr_sects, GFP_NOFS, 1);
 	return 1;
 }
 
@@ -262,7 +270,7 @@ static int secdel_end_io(struct dm_target *ti, struct bio *bio,
 static void secdel_status(struct dm_target *ti, status_type_t type,
 			  unsigned status_flags, char *result, unsigned maxlen)
 {
-	struct secdel_c *lc = (struct secdel_c *) ti->private;
+	struct secdel_c *lc = ti->private;
 
 	switch (type) {
 	case STATUSTYPE_INFO:
@@ -283,7 +291,7 @@ static int secdel_prepare_ioctl(struct dm_target *ti, struct block_device **bdev
 #endif
 		)
 {
-	struct secdel_c *lc = (struct secdel_c *) ti->private;
+	struct secdel_c *lc = ti->private;
 	struct dm_dev *dev = lc->dev;
 
 	*bdev = dev->bdev;

commit 49b20b1adfa1f232c12f7b9c11228f13d130814d

Fix crash since v5.16
In cf6d6238cdd3 ("block: turn macro helpers into inline functions")
(which is v5.16-rc1~74^2~66) bio_set_dev define replaced with inline
function causing extra old fallback to activate. Make 'ifndef'
conditional for `< 4.15' (to cover lower versions end, such as
v4.14-rc1~81^2~49) and not '< 5.16' in case this change is backported
into stable branches.

  [    1.979118] BUG: kernel NULL pointer dereference, address: 000000b0
  [    1.979599] #PF: supervisor read access in kernel mode
  [    1.979983] #PF: error_code(0x0000) - not-present page
  [    1.980365] *pde = 00000000
  [    1.980594] Oops: 0000 [#1] PREEMPT SMP
  [    1.980885] CPU: 7 PID: 183 Comm: udevd Tainted: G           OE     5.19.16-un-def-alt2 #1
  [    1.981502] Hardware name: QEMU Standard PC (i440FX + PIIX, 1996), BIOS 1.16.0-alt1 04/01/2014
  [    1.982149] EIP: submit_bio_noacct+0x139/0x5a0
  [    1.982490] Code: 90 8b 43 38 85 c0 74 0d 8b 15 c0 14 3d c2 8b 84 90 90 00 00 00 80 7b 0c 00 79 09 f6 80 ac 00 00 00 04 74 2d 8b 53 08 83 e2 01 <80> bc 10 b0 00 00 00 00 74 1d 89 d8 e8 d6 9b 02 00 84 c0 0f 85 35
  [    1.983890] EAX: 00000000 EBX: c4bb9f58 ECX: 00031f80 EDX: 00000000
  [    1.984358] ESI: c3a56980 EDI: c367dba0 EBP: c3e25ac8 ESP: c3e25a6c
  [    1.984835] DS: 007b ES: 007b FS: 00d8 GS: 0033 SS: 0068 EFLAGS: 00210246
  [    1.985344] CR0: 80050033 CR2: 000000b0 CR3: 04351000 CR4: 00000690
  [    1.985819] Call Trace:
  [    1.986011]  ? dm_io_acct+0x5a/0x160 [dm_mod]

Fixes: 6d8f9f8 ("Compatibility with v4.13 and v4.14")
Signed-off-by: Vitaly Chikunov <vt@altlinux.org>

dm-secdel.c
@@ -139,8 +139,10 @@ static sector_t secdel_map_sector(struct dm_target *ti, sector_t bi_sector)
 	return lc->start + dm_target_offset(ti, bi_sector);
 }
 
-#ifndef bio_set_dev
-#define bio_set_dev(bio, x) (bio)->bi_bdev = (x)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,15,0)
+# ifndef bio_set_dev
+# define bio_set_dev(bio, x) (bio)->bi_bdev = (x)
+# endif
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,13,0)